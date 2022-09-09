a811181f6f4b2ba7ec764a3c539bb0047780f74f
HuyVo <tuhuy286@gmail.com>
2022-09-09 11:16:42 +0700

    init app

---
+++ app/graphql/queries/fetch_posts.rb
@@ -0,0 +1,9 @@
+module Queries
+  class FetchPosts < Queries::BaseQuery
+    type [Types::PostType], null: false
+
+    def resolve
+      Post.all.order(created_at: :desc)
+    end
+  end
+end
---
+++ app/views/comments/_comment.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! comment, :id, :body, :Post, :created_at, :updated_at
+json.url comment_url(comment, format: :json)
