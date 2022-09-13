e12c1c66cb3cbb2bf7dbb58c9800bedde34ac916
HuyVo <tuhuy286@gmail.com>
2022-09-13 16:31:05 +0700

    graphql with lookahead

--- app/graphql/queries/fetch_posts.rb
+++ app/graphql/queries/fetch_posts.rb
@@ -1,9 +1,14 @@
 module Queries
   class FetchPosts < Queries::BaseQuery
     type [Types::PostType], null: false
+    extras [:lookahead]
 
-    def resolve
-      Post.all.order(created_at: :desc)
+    def resolve(lookahead:)
+      posts = Post.all.order(created_at: :desc)
+
+      return posts unless lookahead.selects?(:comments)
+
+      posts.includes(:comments)
     end
   end
 end
--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -4,7 +4,7 @@ module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
     field :title, String
-    field :comments, [Types::CommentType], null: false, extras: [:lookahead]
+    field :comments, [Types::CommentType], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
