ded6860c88a2c3a6893a36dc62ff0b60b3414c34
HuyVo <tuhuy286@gmail.com>
2022-10-04 17:18:56 +0700

    update #5

--- app/graphql/types/comment_type.rb
+++ app/graphql/types/comment_type.rb
@@ -3,8 +3,8 @@
 module Types
   class CommentType < Types::BaseObject
     field :id, ID, null: false
-    # field :body, String
-    # field :post_id, Integer, null: false
+    field :body, String
+    field :post_id, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
