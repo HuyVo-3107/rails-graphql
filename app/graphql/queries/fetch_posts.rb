b08d52b9bebed51abfd2a76b8b24a2a5025e7612
HuyVo <tuhuy286@gmail.com>
2022-10-04 09:38:25 +0700

    update post query

--- app/graphql/queries/fetch_posts.rb
+++ app/graphql/queries/fetch_posts.rb
@@ -3,14 +3,12 @@ module Queries
     description "Fetch list posts"
 
     type [Types::PostType], null: false
-    extras [:lookahead]
+    # extras [:lookahead]
 
     def resolve(lookahead:)
       # puts lookahead.arguments
       # posts = Post.order(created_at: :desc)
-      #
       # return posts unless lookahead.selects?(:comments)
-      #
       # posts.includes(:comments)
 
       ::Post.all
